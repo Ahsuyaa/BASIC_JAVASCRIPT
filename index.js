70109f618abf9963a9883d5caac46951c6e90d20
aayusha <ayusharaut395@gmail.com>
2023-01-08 02:40:03 -0800

    add is if/else and switch

--- index.js
+++ index.js
@@ -75,5 +75,149 @@ const heading = document.getElementById("mycss");
  heading.style.background="red";
  heading.style.color="white";
  heading.style.textAlign= "center";
+ //condition(if/else)
+//  let email ="ayusharaut395@gmail.com";
+//  let password ="aayusha";
+//  if(email==="ayusharaut395@gmail.com"&& password==="aayusha")
+//  {
+//     console.log("login success");
+//  }
+// else 
+// {
+//     console.log("invalid credential");
+// }
+// student grade
+// let grade = "A";
 
+// if (grade==="B")
+// {
+//     console.log("very good")
 
+// }
+// else if (grade==="c")
+// {
+//     console.log("do better");
+
+// }
+// else if(grade==="d")
+// {
+//     console.log("need improvement");
+// }
+// else if(grade==="A")
+// {
+//     console.log("excellent");
+// }
+// else
+// {
+//     console.log("invalid")
+// }
+// let x= 10;
+// if (x>15)
+// {
+//     xonsole.log("x is greter than 15");
+// }
+// else if (x>5)
+// {
+//     console.log("x is greater than 5 but not greater 15");
+// }
+// else
+// {
+//     console.log(" x is not geater than 15");
+// }
+//weather
+// let weather ="winter";
+// if (weather==="sunny")
+// {
+//     console.log("its sunny day");
+// }
+// else if(weather==="cloudy")
+
+// {
+//     console.log("its going to rain")
+// }
+// else if(weather==="winter")
+// {
+//     console.log("its too cold")
+// }
+// else
+// {
+//     console.log("invalid");
+// }
+//switch case
+// let day ="sunday"
+// switch (day) {
+//     case "monday":
+//         console.log("today is monday");
+//         break;
+//     case "tuesday":
+//       console.log("today is tuesday");
+
+//      case "wednesday":
+//         console.log("today is wednesday");
+//         break;
+//     case "thursday":
+//         console.log("today is thursday");
+//         break;
+//      case "friday":
+//             console.log("today is friday");
+//             break;
+//       case "saturday":
+//                 console.log("today is saturday");
+//                 break;
+//     case "sunday":
+//         console.log("today is sunday");
+//         break;
+//     default:
+//         console.log("error");
+//         break;
+// }
+// let grade ="A";
+// switch(grade)
+// {
+//     case "B":
+//         console.log("very good");
+//         break;
+//      case "c":
+//          console.log("better");
+//          break;
+//      case "D":
+//          console.log("improvement needed");
+//          break;
+//      case "E":
+//         console.log("you are failed");
+//         break;
+//       case "A":
+//          console.log("aich topper");
+//          break;
+//     default:
+//         console.log("error");
+//         break;
+        
+// }
+let month ="feb";
+switch(month)
+{
+    case "jan":
+    case "feb":
+    case "march":
+        console.log("here is first three month");
+        break;
+     case "april":
+     case "may":
+     case "june":
+                console.log("month start with april and end with june");
+                break;
+     case "july":
+     case "aug":
+     case "sep":
+                 console.log("here month start with july and end with sep ");
+                 break;
+     case "oct":
+     case "nov":
+     case "dec":
+                  console.log("last trimester");
+                  break;
+            default:
+                console.log("error");
+                break;
+}
